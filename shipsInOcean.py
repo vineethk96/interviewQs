1ea7f111fe1531d313ad51cc86036303cb2a8a75
Vineeth Kirandumkara <vineethk@vt.edu>
2020-11-13 14:10:22 -0500

    initial recursion algo written

--- shipsInOcean.py
+++ shipsInOcean.py
@@ -26,7 +26,10 @@
 # hasShip(A, C) = True
 # hasShip(A, D) = True
 # hasShip(E, B) = False
-# hasShip(D, B) = False
+# hasShip(D, B) = 
+
+# Global Variables
+shipCount = 0
 
 def hasShip(coord1, coord2):
 
@@ -45,13 +48,39 @@ def hasShip(coord1, coord2):
 
 
 def countShips(coord1, coord2):
-    
+
+    if hasShip(coord1, coord2) and coord1 != coord2:
+
+        diffx = coord2[0] - coord1[0]
+        diffy = coord2[1] - coord1[1]
+        
+        halfCoord = (coord1[0] + diffx, coord1[1] + diffy)
+        leftCoord = (coord1[0], halfCoord[1])
+        topCoord = (halfCoord[0], coord2[1])
+        rightCoord = (coord2[0], halfCoord[1])
+        bottomCoord = (halfCoord[0], coord1[1])
+        
+        countShips(coord1, halfCoord)       # Quadrant 3
+        countShips(halfCoord, coord2)       # Quadrant 1
+        countShips(leftCoord, topCoord)     # Quadrant 2
+        countShips(bottomCoord, rightCoord) # Quadrant 4
+
+    elif hasShip(coord1, coord2) and coord1 == coord2:
+        shipCount = shipCount + 1
+
+    else:
+        print("No ships here")
+
+    return shipCount
 
 
 def main():
     print( str( hasShip( (0,0), (3,3) ) ))
     print( str( hasShip( (0,3), (3,3) ) ))
     print( str( hasShip( (0,0), (0,3) ) ))
+    print( str( hasShip( (3,3), (0,0) ) ))
+
+    print( str(countShips((0,0), (3,3))))
 
 
 
