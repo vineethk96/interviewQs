113ab5ea4640084ebd6785622f818bca58c4bf3b
Vineeth Kirandumkara <vineethk@vt.edu>
2020-11-13 13:00:52 -0500

    Question added

---
+++ shipsInOcean.py
@@ -0,0 +1,30 @@
+# Imagine an ocean where ships are marked with the char x.
+# You are provided a function hasShip(a, b) which accepts tuples
+# This function returns a boolean on if a ship exists within 
+# the two coordinates.
+
+# Goal: create a function countShips(a, b) which leverages 
+# hasShip to determine the amount of ships in the ocean.
+
+'''
+|E|x|x|A|
+---------
+| |C| |x|
+---------
+| | | |D|
+---------
+|B| | | |
+'''
+
+# hasShip(A, C) = True
+# hasShip(A, D) = True
+# hasShip(E, B) = False
+# hasShip(D, B) = False
+
+def main():
+
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
