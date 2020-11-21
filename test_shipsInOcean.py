b4216e4b6bf08668c87e4ece4a098259f58ae549
Vineeth Kirandumkara <vineethk@vt.edu>
2020-11-21 14:28:46 -0500

    added tests for countships

--- test_shipsInOcean.py
+++ test_shipsInOcean.py
@@ -16,3 +16,20 @@ def test_hasShip():
     assert hasShip(C, B) == True
     assert hasShip(D, B) == True
     assert hasShip(D, C) == True
+
+def test_countShips():
+
+    A = (0,0)
+    B = (3,3)
+    C = (3,1)
+    D = (0,2)
+    E = (0,3)
+
+    assert countShips(A, B) == 3
+    assert countShips(A, C) == 0
+    assert countShips(A, E) == 0
+    assert countShips(E, B) == 2
+    assert countShips(C, B) == 1
+    assert countShips(D, B) == 3
+    assert countShips(D, C) == 1
+    
